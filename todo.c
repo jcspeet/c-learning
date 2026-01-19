6a94319a347aababfa8ff2c723f1b6d45d8bd96f
Jarmo <jason.speet@gmail.com>
2026-01-19 05:58:09 -0500

    mac changes

--- todo.c
+++ todo.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-
-
-
-
 int main() {
-
+    int choice;
     printf("1. Add a task\n");
     printf("2. View all tasks\n");
     printf("3. Delete a task\n");
     printf("4. Exit\n");
+    scanf("%d", &choice);
+
+    printf("You chose %d\n", choice);
+
 
 }
 
