8d1db4a6211f9708aea602ebc871dab225638918
Jarmo <jason.speet@gmail.com>
2026-01-18 22:26:02 -0500

    Very good chagnges#

---
+++ todo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+
+
+int main() {
+
+    printf("1. Add a task\n");
+    printf("2. View all tasks\n");
+    printf("3. Delete a task\n");
+    printf("4. Exit\n");
+
+}
+
+
